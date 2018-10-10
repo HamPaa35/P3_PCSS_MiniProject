b6f0b5329fc44d17509b7554fba971b6f8c573fd
Caseguy <33653535+Caseguy@users.noreply.github.com>
2018-10-10 10:55:02 +0200

    Made a program about KASPER

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # P3_PCSS_MiniProject
+
+
+Mathias er en fag bag
--- test.cpp
+++ test.cpp
@@ -2,3 +2,17 @@
 // Created by mngf1 on 10-10-2018.
 //
 
+
+int main(){
+
+    string offens;
+
+    cin >> offens;
+
+    if(offens == "Kasper" ){
+        cout << "This dude is an Idiot!" << endl;
+    }
+
+    return 0;
+}
+
